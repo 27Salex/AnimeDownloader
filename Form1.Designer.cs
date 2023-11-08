64a94b863b5d22b6d384e200203c0ac4cf002b31
27Salex <A7lm27@gmail.com>
2023-11-08 02:06:37 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AnimeDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAnimeName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAutoDownload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAnimeName
+            // 
+            this.txtAnimeName.Location = new System.Drawing.Point(54, 74);
+            this.txtAnimeName.Name = "txtAnimeName";
+            this.txtAnimeName.Size = new System.Drawing.Size(245, 20);
+            this.txtAnimeName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter anime name";
+            // 
+            // btnAutoDownload
+            // 
+            this.btnAutoDownload.Location = new System.Drawing.Point(374, 116);
+            this.btnAutoDownload.Name = "btnAutoDownload";
+            this.btnAutoDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnAutoDownload.TabIndex = 2;
+            this.btnAutoDownload.Text = "download";
+            this.btnAutoDownload.UseVisualStyleBackColor = true;
+            this.btnAutoDownload.Click += new System.EventHandler(this.btnAutoDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAutoDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAnimeName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAnimeName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAutoDownload;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace AnimeDownloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAutoDownload_Click(object sender, EventArgs e)
+        {
+            var driver = new ChromeDriver();
+            try
+            {
+                driver.Url = "https://animefenix.tv/user/login";
+
+                var txtUsername = new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+    .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("username")));
+                var txtPwd = new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+    .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("password")));
+                var btnSend = new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+    .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("send_button")));
+
+                txtUsername.SendKeys("animeweb");
+                txtPwd.SendKeys("Alm.27.01.06");
+                btnSend.Click();
+
+                while (!driver.Url.Contains("zerotwo"))
+                    Thread.Sleep(10);
+
+                driver.Url = "https://animefenix.tv/ver/" + txtAnimeName.Text + "-1/descarga";
+
+                while (!driver.Url.Contains(txtAnimeName.Text)) 
+                    Thread.Sleep(10);
+
+                var burstCloudBtn = new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+    .Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.PartialLinkText("burstcloud")));
+                burstCloudBtn.Click();
+
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                driver.Quit();
+                throw;
+            }
+        }
+    }
+}
